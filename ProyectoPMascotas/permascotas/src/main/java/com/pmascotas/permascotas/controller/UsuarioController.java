598ff9e3d23cf3c9b9a6e8eabddd9bd0d294184c
Daniel López Correa <87211525+lcdaniel04@users.noreply.github.com>
2022-09-26 19:44:22 -0500

    cambios en servicio usuario

--- ProyectoPMascotas/permascotas/src/main/java/com/pmascotas/permascotas/controller/UsuarioController.java
+++ ProyectoPMascotas/permascotas/src/main/java/com/pmascotas/permascotas/controller/UsuarioController.java
@@ -1,9 +1,11 @@
 package com.pmascotas.permascotas.controller;
 
-import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,18 +17,29 @@ import com.pmascotas.permascotas.service.UsuarioSevice;
 @RestController
 @RequestMapping("/user")
 public class UsuarioController {
-
     @Autowired
     UsuarioSevice usuarioSevice;
 
-    @GetMapping()
-    public ArrayList<Usuario> obtenerUsuarios() {
-         return usuarioSevice.obtenerUsuarios();
-    }
-
+    // Crear usuario
     @PostMapping()
-    public Usuario guardarUsuarios(@RequestBody Usuario usuario) {
+    public Usuario guardarUsuario(@RequestBody Usuario usuario) {
         return usuarioSevice.guardarUsuario(usuario);
     }
-    
+
+    // Consultar usuario
+    @GetMapping( path = "/{idUsuario}")
+    public Optional<Usuario> obtenerUsuarioPorId(@PathVariable("idUsuario") String idUsuario) {
+        return this.usuarioSevice.consultarUsuarioPorId(idUsuario);
+    }
+
+    // Eliminar un usuario
+    @DeleteMapping( path = "/{idUsuario}")
+    public String eliminarUsuarioPorId(@PathVariable("idUsuario") String idUsuario){
+        boolean ok = this.usuarioSevice.eliminarUsuario(idUsuario);
+        if (ok){
+            return "Se eliminó el usuario con id" + idUsuario;
+        }else{
+            return "No pudo eliminar el usuario con id" + idUsuario;
+        }
+    }
 }
